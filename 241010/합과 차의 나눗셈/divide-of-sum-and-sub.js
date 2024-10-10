c1b4630c99fc61eca65e8ad948186fc990ca6b16
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-10 22:12:10 +0900

    Title: 합과 차의 나눗셈, Time: 190ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241010/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+let result = (a + b)/(a - b);
+console.log(result.toFixed(2));
