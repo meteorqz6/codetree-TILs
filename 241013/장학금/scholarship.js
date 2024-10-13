8f1de9c5d41b1a2274a429b1d54eb397ff993b20
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-13 15:35:16 +0900

    Title: 장학금, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241013/장학금/scholarship.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+if(a>=90 && b>=95){
+    console.log(100000);
+} else if(a>=90 && b>=90) {
+    console.log(50000);
+} else {
+    console.log(0);
+}
