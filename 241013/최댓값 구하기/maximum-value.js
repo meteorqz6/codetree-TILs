a1f34e239a9a4b4392a60e28e9b357aacf3eb1dd
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-13 16:11:14 +0900

    Title: 최댓값 구하기, Time: 167ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241013/최댓값 구하기/maximum-value.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+if(a>=b&&a>=c){
+    console.log(a);
+} else if(b>=a&&b>=c){
+    console.log(b);
+} else {
+    console.log(c);
+}
