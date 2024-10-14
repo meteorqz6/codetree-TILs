99783793420d8ac90643c5e1af170532807e765f
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-14 21:46:58 +0900

    Title: 짝수만 출력 2, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241014/짝수만 출력 2/output-only-even-2.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+let i = a;
+let result = "";
+
+while(i>=b){
+    result += ( i + " ");
+    i -= 2;
+}
+console.log(result);
