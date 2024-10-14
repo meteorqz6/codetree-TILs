d74d0c8f395b81e622692ef0c45ffacb2e792dcf
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-14 10:51:47 +0900

    Title: 홀수만 출력 2, Time: 201ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241014/홀수만 출력 2/output-only-odd-2.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ');
+let b = Number(input[0]);
+let a = Number(input[1]);
+let result = "";
+for ( let i = b; i >= a; i -= 2){
+    result += (i + ' ');
+}
+console.log(result);
