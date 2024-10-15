7ec04fd9e07a0de04011ee243de65e895519d24d
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-15 23:55:52 +0900

    Title: 등장횟수 확인, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241015/등장횟수 확인/check-number-of-appearances.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split('\n');
+let cnt = 0;
+for(let i = 0; i < 5; i++){
+    if(Number(input[i])%2===0){
+        cnt++;
+    }
+}
+console.log(cnt);
