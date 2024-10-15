c8f72b6899e52c91e26163659745792f7d8cc8a8
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-15 13:52:02 +0900

    Title: 2 또는 3의 배수, Time: 197ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241015/2 또는 3의 배수/multiple-of-2-or-3.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync(0).toString().trim());
+let result = "";
+for(let i = 1; i <= n; i++){
+    if(i%2===0 || i%3===0){
+        result += (1 + ' ');
+    }
+    else{
+        result += (0 + ' ');
+    }
+}
+console.log(result);
