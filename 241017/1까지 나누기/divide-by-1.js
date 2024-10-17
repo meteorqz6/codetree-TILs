ac5e5a1dfe21d123208b66505f4908e5d13e8d09
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-17 19:40:06 +0900

    Title: 1까지 나누기, Time: 235ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 241017/1까지 나누기/divide-by-1.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync(0).toString().trim());
+let cnt = 0;
+for(let i = 1; ; i++){
+    n = parseInt(n/i);
+    cnt++;
+    if(n<=1){
+        console.log(cnt);
+        break;
+    }
+}
