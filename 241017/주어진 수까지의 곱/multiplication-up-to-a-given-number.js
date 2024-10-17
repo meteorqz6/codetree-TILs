14282d05ecc3f323d6c3107d5ebd45e7eb2e590a
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-17 13:22:59 +0900

    Title: 주어진 수까지의 곱, Time: 240ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241017/주어진 수까지의 곱/multiplication-up-to-a-given-number.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+let prod = 1;
+
+for(let i = a; i<=b; i++){
+    prod *= i;
+}
+console.log(prod);
