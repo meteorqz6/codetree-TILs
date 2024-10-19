59edcc3b14905a6827a6044e219119137e554b9a
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-20 01:07:29 +0900

    Title: ab 사이에 없는 c, Time: 187ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241020/ab 사이에 없는 c/c-between-a-and-b-2.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+let satisfied = 'YES';
+
+for(let i = a; i <= b; i++){
+    if(i%c===0){
+        satisfied = 'No';
+    }
+}
+
+console.log(satisfied);
