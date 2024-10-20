14e853c1be8236804d2f54c8b5163367981bc4ff
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-20 13:36:03 +0900

    Title: 별표 출력하기 2, Time: 180ms, Memory: 23MB, Status: Passed - CodeTree

--- 241020/별표 출력하기 2/print-star-2.js
+++ 241020/별표 출력하기 2/print-star-2.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let n = Number(fs.readFileSync(0).toString().trim());
 let str = "";
 
-for(let i = 5; i > 0; i--){
+for(let i = n; i > 0; i--){
     str = "";
     for(let j = i; j > 0; j--){
         str += '* ';
