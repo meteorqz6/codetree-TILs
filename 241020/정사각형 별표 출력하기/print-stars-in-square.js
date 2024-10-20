6d4062f0244a92fc74a6ae835f5234bd7d22d107
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-20 11:04:24 +0900

    Title: 정사각형 별표 출력하기, Time: 226ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241020/정사각형 별표 출력하기/print-stars-in-square.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync(0).toString().trim());
+
+for(let i = 0; i < n; i++) {
+    let str = "";
+    for(let j = 0; j < n; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
