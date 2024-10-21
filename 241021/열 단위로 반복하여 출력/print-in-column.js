e330fe91aa1ec2033886b36093585caa7a43a828
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-21 22:41:54 +0900

    Title: 열 단위로 반복하여 출력, Time: 164ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241021/열 단위로 반복하여 출력/print-in-column.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+let str = "";
+
+for(let i = 1 ; i <= n; i++){
+    str = "";
+    for(let j = 1; j <= n; j++){
+        str += i;
+    }
+    console.log(str);
+}
