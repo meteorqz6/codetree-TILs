a47aaeefe7411b13da5c4cedb7803241b6207f81
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-26 02:25:02 +0900

    Title: 부분 문자열의 개수, Time: 229ms, Memory: 15MB, Status: Passed - CodeTree

--- 241026/부분 문자열의 개수/number-of-substrings.js
+++ 241026/부분 문자열의 개수/number-of-substrings.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const strA = input[0];
 const strB = input[1];
+const char1 = strB[0];
+const char2 = strB[1];
 let cnt = 0;
-let idx = strA.indexOf(strB);
 
-while(idx!==-1){
-    cnt++;
-    idx = strA.indexOf(strB, idx+2);
+for(let i = 0; i < strA.length - 1; i++){
+    if(char1 === strA[i] && char2 === strA[i+1]){
+        cnt++;
+    }
 }
 console.log(cnt);
