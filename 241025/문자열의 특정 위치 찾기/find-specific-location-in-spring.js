136b222dd06daa11f2a8eb9f8f377a6dbfd12935
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-25 12:23:43 +0900

    Title: 문자열의 특정 위치 찾기, Time: 175ms, Memory: 14MB, Status: Passed - CodeTree

--- 241025/문자열의 특정 위치 찾기/find-specific-location-in-spring.js
+++ 241025/문자열의 특정 위치 찾기/find-specific-location-in-spring.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString().split(' ');
+const input = fs.readFileSync(0).toString().trim().split(' ');
 const str = input[0];
 const char = input[1];
 if(str.indexOf(char)===-1){
