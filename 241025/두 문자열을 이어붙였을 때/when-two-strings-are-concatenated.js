9d15542a0f683628576399f9522f10b6ec885b42
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-25 11:55:38 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 278ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241025/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().split('\n');
+const a = input[0];
+const b = input[1];
+const ab = a + b;
+const ba = b + a;
+
+if(ab === ba){
+    console.log('true');
+} else {
+    console.log('false');
+}
