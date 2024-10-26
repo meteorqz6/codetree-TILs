b3935dda8f8f0eaa234e6192c8a2d3cd18d8293a
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-26 14:12:43 +0900

    Title: 첫 번째와 두 번째 교환, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241026/첫 번째와 두 번째 교환/exchange-1st-and-2nd.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+const firstChar = input[0];
+const secondChar = input[1];
+const arr = input.split('');
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] === firstChar){
+        arr[i] = secondChar;
+    } else if(arr[i] === secondChar){
+        arr[i] = firstChar;
+    }
+}
+console.log(arr.join(''));
