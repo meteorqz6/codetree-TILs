c07c954b898f7e81ac3f7dc2656cc0e38d02b3e0
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-29 09:52:51 +0900

    Title: 그 다음 알파벳, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

--- 241029/그 다음 알파벳/next-alphabet.js
+++ 241029/그 다음 알파벳/next-alphabet.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
 const n = input.charCodeAt() + 1;
 if(input==='z'){
-    console.log(String.fromCharCode(33));
+    console.log(String.fromCharCode(97));
 } else {
     console.log(String.fromCharCode(n));
 }
