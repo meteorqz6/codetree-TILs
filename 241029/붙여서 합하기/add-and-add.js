ccee5fc1e8fd43fe4b3f50a9da1953a3e2037bb4
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-29 14:02:26 +0900

    Title: 붙여서 합하기, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241029/붙여서 합하기/add-and-add.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(' ');
+const AB = input[0] + input[1];
+const BA = input[1] + input[0];
+console.log(Number(AB)+Number(BA));
