db73cd58e1a1a9431437e3489e5c03fbd6f6586d
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-30 15:45:50 +0900

    Title: 배열 채우기, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241030/배열 채우기/filling-array.js
+++ 241030/배열 채우기/filling-array.js
@@ -3,7 +3,7 @@ let input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 let arr = [];
 
 for(let i = input.length - 1; i >= 0; i--){
-    if(input[i]!=='0'){
+    if(input[i]!==0){
         arr.push(input[i]);
     }
 }
