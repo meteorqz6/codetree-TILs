18886893c93d14867107d73cd18c538d45e89984
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-10-30 20:50:45 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 251ms, Memory: 14MB, Status: Passed - CodeTree

--- 241030/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.js
+++ 241030/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.js
@@ -4,6 +4,9 @@ let cnt = 0;
 let sum = 0;
 
 for(let i = 0; i < input.length; i++){
+    if(input[i] === 0){
+        break;
+    }
     if(input[i]!==0 && input[i]%2===0){
         sum += input[i];
         cnt++;
