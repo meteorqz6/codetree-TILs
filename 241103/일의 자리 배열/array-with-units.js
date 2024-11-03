c802cfb0666677e9bdaceff3c581344fe6b25fb3
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-03 18:31:06 +0900

    Title: 일의 자리 배열, Time: 171ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241103/일의 자리 배열/array-with-units.js
@@ -0,0 +1,12 @@
+let arr = Array(10);
+
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+arr[0] = input[0];
+arr[1] = input[1];
+
+for(let i = 2; i < 10; i++){
+    arr[i] = (arr[i-1] + arr[i-2])%10;
+}
+
+console.log(arr.join(' '));
