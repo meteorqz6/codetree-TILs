bb9a5660412b17416179628104b2e6c750bdb40b
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-05 00:22:51 +0900

    Title: 특정 위치의 문자, Time: 160ms, Memory: 14MB, Status: Passed - CodeTree

--- 241105/특정 위치의 문자/char-in-specific-location.js
+++ 241105/특정 위치의 문자/char-in-specific-location.js
@@ -1,13 +1,17 @@
 const word = ['L', 'E', 'B', 'R', 'O', 'S'];
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
+let idx = -1;
 
 for(let i = 0; i < word.length; i++) {
     if(input===word[i]) {
-        console.log(i);
-        break;
-    } else {
-        console.log('None');
-        break;
+        idx = i;
     }
+}
+
+if(idx === -1) {
+    console.log('None');
+}
+else {
+    console.log(idx);
 }
