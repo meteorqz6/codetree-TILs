9deccc4d9ef8cce8dfe57ed6e3434805c2b9ed94
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-06 14:06:26 +0900

    Title: n개의 숫자 중 최대 2개, Time: 162ms, Memory: 14MB, Status: Passed - CodeTree

--- 241106/n개의 숫자 중 최대 2개/two-max-of-n-num.js
+++ 241106/n개의 숫자 중 최대 2개/two-max-of-n-num.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
-const sortArr = input.sort((a,b) => b-a);
-console.log(sortArr);
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const arr = input[1].split(' ').map(Number);
+const sortArr = arr.sort((a,b) => b-a);
+console.log(sortArr[0], sortArr[1]);
