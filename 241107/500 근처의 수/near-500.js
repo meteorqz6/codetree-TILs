2f1f61dbe5ccee83dfcaef9fec27642ceab3695c
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-07 23:46:48 +0900

    Title: 500 근처의 수, Time: 154ms, Memory: 14MB, Status: Runtime Error - CodeTree

---
+++ 241107/500 근처의 수/near-500.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().map(Number);
+let limitUnder500 = [];
+let limitUpper500 = [];
+
+for(let i = 0; i < 10; i++) {
+    if(input[i] > 500) {
+        limitUpper500.push(input[i]);
+    } else {
+        limitUnder500.push(input[i]);
+    }
+}
+
+const max = Math.max(...limitUnder500);
+const min = Math.min(...limitUpper500);
+
+console.log(max, min);
