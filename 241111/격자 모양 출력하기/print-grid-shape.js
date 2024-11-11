ad529d189eb705a708615a1fdb2bd748f5726aee
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-11 11:12:21 +0900

    Title: 격자 모양 출력하기, Time: 216ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241111/격자 모양 출력하기/print-grid-shape.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0].split(' ')[0]);
+const m = Number(input[0].split(' ')[1]);
+
+let arr2d = Array(n).fill(0).map(() => Array(n).fill(0));
+
+for (let i = 1; i <= m; i++) {
+    let r = Number(input[i].split(' ')[0]);
+    let c = Number(input[i].split(' ')[1]);
+    arr2d[r - 1][c - 1] = r * c;
+}
+
+console.log(arr2d.map((row) => row.join(' ')).join('\n'));
