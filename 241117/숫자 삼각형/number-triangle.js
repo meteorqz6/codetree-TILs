34ca6a833b408a6242675cb32496a5286a95ced6
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-17 20:03:40 +0900

    Title: 숫자 삼각형, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241117/숫자 삼각형/number-triangle.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+let str = '';
+
+for (let i = 0; i < n; i++) {
+    str = '';
+    for (let j = 0; j <= i; j++) {
+        str += j + 1 + ' ';
+    }
+    console.log(str);
+}
