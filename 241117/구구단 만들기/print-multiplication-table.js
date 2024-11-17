3b88de82626efce67d4e12bb5935bfb4ce0a3139
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-17 19:51:45 +0900

    Title: 구구단 만들기, Time: 194ms, Memory: 15MB, Status: Passed - CodeTree

--- 241117/구구단 만들기/print-multiplication-table.js
+++ 241117/구구단 만들기/print-multiplication-table.js
@@ -4,9 +4,9 @@ let str = '';
 
 for (let i = 1; i <= 9; i++) {
     str = '';
-    for (let j = input[1]; j >= 2; j -= 2) {
+    for (let j = input[1]; j >= input[0]; j -= 2) {
         str += `${j} * ${i} = ${j*i} `;
-        if(j!==2) {
+        if(j!==input[0]) {
             str += '/ ';
         }
     }
