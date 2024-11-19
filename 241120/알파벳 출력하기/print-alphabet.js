6d278d88a538ecd6d1aed8aabaa4f9ae24ca2a19
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-20 00:37:04 +0900

    Title: 알파벳 출력하기, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 241120/알파벳 출력하기/print-alphabet.js
+++ 241120/알파벳 출력하기/print-alphabet.js
@@ -6,11 +6,12 @@ let index = 65;
 for (let i = 0; i < n; i++) {
     str = '';
     for (let j = 0; j < i + 1; j++) {
+        str += String.fromCharCode(index);
         if (String.fromCharCode(index) === 'Z') {
             index = 65;
+        } else {
+            index++;
         }
-        str += String.fromCharCode(index);
-        index++;
     }
     console.log(str);
 }
