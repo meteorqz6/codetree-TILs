6f8dada75bc0813aef047682332ca1c2f76f9c6e
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-23 23:33:40 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

--- 241123/중복되지 않는 숫자 중 최대/max-of-unique-number.js
+++ 241123/중복되지 않는 숫자 중 최대/max-of-unique-number.js
@@ -4,8 +4,17 @@ const n = Number(input[0]);
 const arr = input[1].split(' ').map(Number).sort((a, b) => a - b);
 let result = [];
 
-for (let i = 1; i < n; i++) {
-    if (arr[i - 1] !== arr[i] && arr[i] !== arr[i + 1]) {
+for (let i = 0; i < n; i++) {
+    // 첫 번째 요소인 경우
+    if (i === 0 && arr[i] !== arr[i + 1]) {
+        result.push(arr[i]);
+    }
+    // 마지막 요소인 경우
+    else if (i === n - 1 && arr[i] !== arr[i - 1]) {
+        result.push(arr[i]);
+    }
+    // 중간 요소인 경우
+    else if (arr[i] !== arr[i - 1] && arr[i] !== arr[i + 1]) {
         result.push(arr[i]);
     }
 }
