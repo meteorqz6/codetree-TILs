44cea10c0ebd24f0e95a43b91b630d153cfdb13e
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-24 20:00:59 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 192ms, Memory: 14MB, Status: Runtime Error - CodeTree

---
+++ 241124/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let str = input[0].split(' ')[0];
+const len = str.length;
+const n = Number(input[0].split(' ')[1]);
+
+for (let i = 1; i <= n; i++) {
+    let rule = Number(input[i]);
+    if (rule === 1) {
+        str = str.slice(1,len) + str[0];
+        console.log(str);
+    } else if (rule === 2) {
+        str = str[len - 1] + str.slice(0, len - 1);
+        console.log(str);
+    } else {
+        str = str.split('').reverse();
+        console.log(str.join(''));
+    }
+}
