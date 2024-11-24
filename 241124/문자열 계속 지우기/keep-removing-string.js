78c7ddeeb166b89e733e9bb4d1f151ac06db14e6
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-24 20:49:21 +0900

    Title: 문자열 계속 지우기, Time: 177ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241124/문자열 계속 지우기/keep-removing-string.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let strA = input[0];
+let lenA = strA.length;
+const strB = input[1];
+const lenB = strB.length;
+
+while (lenA > lenB) {
+    // console.log(lenA);
+    for (let i = 0; i < lenA - lenB + 1; i++) {
+        let str = strA.slice(i, i + lenB);
+
+        if (str === strB) {
+            let start = i;
+            let end = i + lenB;
+            strA = strA.slice(0, start) + strA.slice(end);
+            lenA = strA.length;
+            break;
+        }
+    }
+}
+
+console.log(strA);
