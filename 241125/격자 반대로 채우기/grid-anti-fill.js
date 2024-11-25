4bcd2e251357119215a4b96bfc9b738060926692
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-25 22:56:58 +0900

    Title: 격자 반대로 채우기, Time: 189ms, Memory: 15MB, Status: Passed - CodeTree

--- 241125/격자 반대로 채우기/grid-anti-fill.js
+++ 241125/격자 반대로 채우기/grid-anti-fill.js
@@ -4,16 +4,31 @@ let arr = Array(n).fill(0).map(() => Array(n).fill(0));
 let num = 1;
 
 for (let i = n - 1; i >= 0; i--) {
-    if (i % 2 === 0) {
-        for (let j = 0; j < n; j++) {
-            arr[j][i] = num;
-            num += 1;
+    if (n % 2 === 0) {
+        if (i % 2 === 0) {
+            for (let j = 0; j < n; j++) {
+                arr[j][i] = num;
+                num += 1;
+            }
+        } else {
+            for (let j = n - 1; j >= 0; j--) {
+                arr[j][i] = num;
+                num += 1;
+            }
         }
     } else {
-        for (let j = n - 1; j >= 0; j--) {
-            arr[j][i] = num;
-            num += 1;
+        if (i % 2 !== 0) {
+            for (let j = 0; j < n; j++) {
+                arr[j][i] = num;
+                num += 1;
+            }
+        } else {
+            for (let j = n - 1; j >= 0; j--) {
+                arr[j][i] = num;
+                num += 1;
+            }
         }
     }
+
 }
 console.log(arr.map((row) => row.join(' ')).join('\n'));
