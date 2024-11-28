f5b5af30c8fa7317df57c6a3ee283c76ae0154af
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-11-28 16:53:03 +0900

    Title: 함수를 이용한 소수 판별, Time: 172ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241128/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
@@ -0,0 +1,21 @@
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ');
+let [a, b] = input.map(Number);
+
+let sum = 0;
+for (let i = a; i <= b; i++) {
+    if (isPrime(i)) {
+        sum += i;
+    }
+}
+
+console.log(sum);
