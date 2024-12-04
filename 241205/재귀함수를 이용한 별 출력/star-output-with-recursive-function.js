2c77915b1ec5a5176090c9aee9da5bc3f6697734
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-12-05 01:24:38 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241205/재귀함수를 이용한 별 출력/star-output-with-recursive-function.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+function printStar(n) {
+    if (n === 0) {
+        return;
+    }
+
+    printStar(n - 1);
+
+    for (let i = 0; i < n; i++) {
+        process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+}
+
+printStar(n);
