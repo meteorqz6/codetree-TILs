106282f5700a68b7191495044e3185a32f44655a
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-12-05 01:31:04 +0900

    Title: 재귀함수의 꽃, Time: 162ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241205/재귀함수의 꽃/the-flower-of-the-recursive-function.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+function print(n) {
+    if (n === 0) {
+        return;
+    }
+    process.stdout.write(n + ' ');
+    print(n - 1);
+    process.stdout.write(n + ' ');
+}
+
+print(n);
