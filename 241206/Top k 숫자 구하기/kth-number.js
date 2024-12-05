bd7324c52ee189e4b6fe250c90b7a5111b83f51f
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-12-06 01:09:00 +0900

    Title: Top k 숫자 구하기, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

--- 241206/Top k 숫자 구하기/kth-number.js
+++ 241206/Top k 숫자 구하기/kth-number.js
@@ -3,5 +3,5 @@ let input = fs.readFileSync(0).toString().trim().split('\n');
 let n = Number(input[0].split(' ')[0]);
 let k = Number(input[0].split(' ')[1]);
 let arr = input[1].split(' ').map(Number);
-arr.sort();
+arr.sort((a, b) => a - b);
 console.log(arr[k - 1]);
