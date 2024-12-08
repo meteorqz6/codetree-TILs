2dcd7bd922cf948c579689a56b5d47dc5e481153
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-12-08 18:51:17 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 247ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 241208/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const str1 = input[0].split('').sort().join('');
+const str2 = input[1].split('').sort().join('');
+if(str1 == str2) {
+    console.log('Yes');
+} else {
+    console.log('No');
+}
