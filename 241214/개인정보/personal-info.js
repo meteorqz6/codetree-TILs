928b58b33972787a8b1bbf1fa7f2f57805f7e516
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-12-14 02:17:51 +0900

    Title: 개인정보, Time: 205ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241214/개인정보/personal-info.js
@@ -0,0 +1,38 @@
+class User {
+    constructor(name, height, weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().split('\n');
+let users = [];
+for (let i = 0; i < 5; i++) {
+    let [name, height, weight] = input[i].split(' ');
+    users.push(new User(name, Number(height), Number(weight)));
+}
+
+// 이름 순 정렬
+users.sort((a, b) => {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+});
+
+
+console.log('name');
+for (let i = 0; i < 5; i++) {
+    console.log(users[i].name, users[i].height, users[i].weight);
+}
+
+users.sort((a, b) => {
+    return b.height - a.height;
+});
+
+console.log();
+console.log('height');
+for (let i = 0; i < 5; i++) {
+    console.log(users[i].name, users[i].height, users[i].weight);
+}
