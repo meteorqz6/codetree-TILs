aeb8ccd6f55200d3618c2a3a699014f09d730edd
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-12-24 13:08:34 +0900

    Title: 그 요일은, Time: 191ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241224/그 요일은/the-day-of-the-day.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().split('\n');
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+const A = input[1];
+
+function num0fDays(m, d) {
+    const days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let totalDays = 0;
+
+    for (let i = 1; i < m; i++) {
+        totalDays += days[i];
+    }
+
+    totalDays += d;
+    return totalDays;
+}
+
+let diff = num0fDays(m2, d2) - num0fDays(m1, d1);
+const dayOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+let cnt = 0;
+let start;
+
+for (let i = 0; i < 7; i++) {
+    if (A === dayOfWeek[i]) {
+        start = i;
+    }
+}
+
+while (true) {
+    cnt++;
+    start += 7;
+    if (start > diff) {
+        break;
+    }
+}
+
+console.log(cnt);
