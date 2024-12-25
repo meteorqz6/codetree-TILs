7162a51f8be346c1022399fbd8176be3e9525af7
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-12-25 15:27:54 +0900

    Title: 왔다 갔던 구역 2, Time: 198ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241225/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+let arr = Array(2001).fill(0);
+let start = 1000;
+
+for (let i = 1; i <= n; i++) {
+    let distance = Number(input[i].split(' ')[0]);
+    let direction = input[i].split(' ')[1];
+
+    if (direction === 'R') {
+        for (let i = start; i < start + distance; i++) {
+            arr[i] += 1;
+        }
+        start = start + distance;
+    } else {
+        for (let i = start; i > start - distance; i--) {
+            arr[i] += 1;
+        }
+        start = start - distance;
+    }
+}
+
+let cnt = 0;
+for (let i = 0; i < 2001; i++) {
+    if (arr[i] > 1) { cnt++; }
+}
+
+console.log(cnt);
