a40f290be4e866081284255583256d4b0a54acdd
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-12-27 20:43:04 +0900

    Title: 사각형의 총 넓이 2, Time: 192ms, Memory: 15MB, Status: Passed - CodeTree

--- 241227/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
+++ 241227/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = Array(201).fill(0).map(() => Array(201).fill(0));
-
+const OFFSET  = 100;
 let cnt = 0;
 
 for (let i = 1; i <= n; i++) {
     let [x1, y1, x2, y2] = input[i].split(' ').map(Number);
 
-    for (let j = x1; j < x2; j++) {
-        for (let k = y1; k < y2; k++) {
+    for (let j = x1 + OFFSET; j < x2 + OFFSET; j++) {
+        for (let k = y1 + OFFSET; k < y2 + OFFSET; k++) {
             arr[j][k] = 1;
         }
     }
