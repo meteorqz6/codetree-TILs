93f8b5198147e63413ee9974df1c24d835d28265
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2025-01-06 19:12:03 +0900

    Title: 가장 많은 데이터, Time: 168ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250106/가장 많은 데이터/most-frequent-data.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const freq = new Map();
+let max = 0;
+
+for (let i = 1; i <= n; i++) {
+    let string = input[i];
+
+    if (freq.has(string)) {
+        freq.set(string, freq.get(string) + 1);
+
+        if (max < freq.get(string)) {
+            max = freq.get(string);
+        }
+    } else {
+        freq.set(string, 1);
+    }
+}
+
+console.log(max);
