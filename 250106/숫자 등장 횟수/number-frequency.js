11cd1ad2f00955bfe5f0a0f1ff3ba1f19f3cbbf2
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2025-01-06 12:13:03 +0900

    Title: 숫자 등장 횟수, Time: 242ms, Memory: 27MB, Status: Passed - CodeTree

--- 250106/숫자 등장 횟수/number-frequency.js
+++ 250106/숫자 등장 횟수/number-frequency.js
@@ -1,17 +1,3 @@
-// const fs = require("fs");
-// const input = fs.readFileSync(0).toString().trim().split("\n");
-
-// const n = Number(input[0]);
-// const arr = input[1].split(" ").map(Number);
-// const k = Number(input[2]);
-
-// const numToIndex = new Map();
-
-// for (let i = 0; i < arr.length; i++)
-// 	numToIndex.set(arr[i], i + 1);
-
-// console.log(numToIndex.get(k));
-
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
@@ -20,17 +6,24 @@ const arr = input[1].split(' ').map(Number);
 const targetArr = input[2].split(' ').map(Number);
 let result = [];
 
-const numToIndex = new Map();
+const numCount = new Map();
 
 for (let i = 0; i < n; i++) {
-    numToIndex.set(arr[i], i + 1);
+    let k = arr[i];
+
+    if (numCount.has(k)) {
+        let v = numCount.get(k);
+        numCount.set(k, v + 1);
+    } else {
+         numCount.set(k, 1);
+    }
 }
 
 for (let i = 0; i < m; i++) {
     let k = targetArr[i];
 
-    if (numToIndex.has(k)) {
-        result.push(numToIndex.get(k));
+    if (numCount.has(k)) {
+        result.push(numCount.get(k));
     } else {
         result.push(0);
     }
